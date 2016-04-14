674ea0fd5a470675f51468e3aaeb67aa7cbb8ebd
andreip1123 <andrei.popa1123@gmail.com>
2016-04-14 11:32:58 +0300

    gulpfile updated

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,10 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 
-gulp.task('uglify', function() {
+gulp.task('prepare-package', function() {
     return gulp.src(['web/github.js', 'web/plugin.js'])
         .pipe(concat('plugin.js'))
         .pipe(uglify())
         .pipe(gulp.dest('target/'));
 });
-gulp.task('default', ['uglify']);
+gulp.task('default', ['prepare-package']);
